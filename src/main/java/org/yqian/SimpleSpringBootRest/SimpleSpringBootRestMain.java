423c97c90a72da13ec707745f20c56d247399fb9
yqian <yi.qian@att.net>
2017-08-22 11:53:53 -0400

    Create SimpleSpringBootRestMain.java

---
+++ src/main/java/org/yqian/SimpleSpringBootRest/SimpleSpringBootRestMain.java
@@ -0,0 +1,3 @@
+package org.yqian.SimpleSpringBootRest;
+import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplicationpublic class SimpleSpringBootRestMain { public static void main(String[] args) {  SpringApplication.run(SimpleSpringBootRestMain.class, args); }}
